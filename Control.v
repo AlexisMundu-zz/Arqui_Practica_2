c4cef31d9ca007a51396fc00b8af2465d05b00ac
Alexis Muñoz <alexismun53@gmail.com>
2020-03-30 17:47:08 -0600

    Add LW function

--- Control.v
+++ Control.v
@@ -33,6 +33,7 @@ localparam I_Type_ANDI = 6'hc;
 localparam I_Type_BEQ = 6'h4;
 localparam I_Type_BNE = 6'h5;
 localparam I_Type_LUI = 6'hf;
+localparam I_Type_LW = 6'h23;
 localparam J_Type_J = 6'h2;
 
 
@@ -47,6 +48,7 @@ always@(OP) begin
 		I_Type_BEQ:		ControlValues= 12'b0_0_000_00_01_001;
 		I_Type_BNE:		ControlValues= 12'b0_0_000_00_10_001;
 		I_Type_LUI:		ControlValues= 12'b0_0_101_00_00_011;
+		I_Type_LW:		ControlValues= 12'b0_0_111_10_00_100;
 		J_Type_J:		ControlValues= 12'b1_0_000_00_00_000;
 		default:
 			ControlValues= 10'b0000000000;
