1efa7d99fa6d5e199e793e2685d5b67ed019659c
Alexis Muñoz <alexismun53@gmail.com>
2020-04-19 17:54:50 -0500

    Create Register_MEM_WB

--- Register_MEM_WB.v
+++ Register_MEM_WB.v
@@ -1,6 +1,6 @@
 /******************************************************************
 * Description
-*	This is a register that corresponds to the EX/MEM. 
+*	This is a register that corresponds to the MEM/WB. 
 *	This register does not have an enable signal.
 * Version:
 *	1.0
@@ -19,33 +19,18 @@ module Register_EX_MEM
 (
 	input clk,
 	input reset,
-	input Zero,
-	input [N-1:0] ALU_result,
-	input [N-1:0] Data_2,
-	input [N-1:0] Jump_address,
-	input	[N-1:0] Branch_adress,
+	input [N-1:0] ALU_result_out,
+	input [N-1:0] Read_data,
 	input [4:0] WriteRegister, 	
 	//Control
-	input Jump,
-	input BranchEQ,
-	input BranchNE,
-	input MemRead,
-	input MemWrite,
 	input MemtoReg,
 	input RegWrite,
 	
 	
 	output reg [N-1:0] ALU_result_out,
-	output reg [N-1:0] Data_2_out,
-	output reg [N-1:0] Jump_address_out,
-	output reg	[N-1:0] Branch_adress_out,
+	output reg [N-1:0] Read_data_out,
 	output reg [4:0] WriteRegister_out, 	
 	//Control
-	output reg Jump_out,
-	output reg BranchEQ_out,
-	output reg BranchNE_out,
-	output reg MemRead_out,
-	output reg MemWrite_out,
 	output reg MemtoReg_out,
 	output reg RegWrite_out
 );
@@ -54,33 +39,19 @@ always@(negedge reset or negedge clk) begin
 	if(reset==0)
 		begin
 			ALU_result_out <= 0;
-			Data_2_out <= 0;
-			Jump_address_out <= 0;
-			Branch_adress_out <= 0;
+			Read_data_out <= 0;
 			WriteRegister_out <= 0;
 			//Control
-			Jump_out <= 0;
-			BranchEQ_out <= 0;
-			BranchNE_out <= 0;
-			MemRead_out <= 0;
 			MemtoReg_out <= 0;
-			MemWrite_out <= 0;
 			RegWrite_out <= 0;
 		end
 	else	
 		begin
 			ALU_result_out <= ALU_result;
-			Data_2_out <= Data_2;
-			Jump_address_out <= Jump_address;
-			Branch_adress_out <= Branch_adress;
+			Read_data_out <= Read_data;
 			WriteRegister_out <= WriteRegister;
 			//Control
-			Jump_out <= Jump;
-			BranchEQ_out <= BranchEQ;
-			BranchNE_out <= BranchNE;
-			MemRead_out <= MemRead;
 			MemtoReg_out <= MemtoReg;
-			MemWrite_out <= MemWrite;
 			RegWrite_out <= RegWrite;
 		end
 end
