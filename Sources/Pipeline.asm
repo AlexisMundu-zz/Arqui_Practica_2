22a98547a65925cf2598c932a7f243e8418d49e7
Alexis Muñoz <alexismun53@gmail.com>
2020-04-19 14:07:53 -0500

    Create IF/ID register

--- Sources/Pipeline.asm
+++ Sources/Pipeline.asm
@@ -5,11 +5,4 @@
 	add $zero, $zero, $zero
 	add $zero, $zero, $zero
 	add $t1, $t0, $zero
-	beq $t0, $t1, branchEQ
-	j end
-branchEQ:
-	addi $t0, $zero, 5000
-	bne $t0, $t1, branchNE
-	j end
-branchNE: addi $t0, $zero, 1000
 end:
